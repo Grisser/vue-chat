18b5c6c3ea0b8804e8a4adaa89e8ad3c20b704e8
Вячеслав Рыжков <rugrisser@gmail.com>
2019-12-21 14:19:14 +0300

    Example

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
     <div id="app">
+        <Container>
+            <ChatWindow>
+                <ChatMessage
+                        username="Ivan"
+                        datetime="21.12.2019 05:00:50">
+                    Hello, World!
+                </ChatMessage>
+            </ChatWindow>
+        </Container>
     </div>
 </template>
 
 <script>
-
+    import Container from "@/components/Container";
+    import ChatMessage from "@/components/ChatMessage";
+    import ChatWindow from "@/components/ChatWindow";
 
     export default {
         name: 'app',
         components: {
-
+            Container, ChatMessage, ChatWindow
         }
     }
 </script>
 
 <style>
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
+    body {
+        margin: 0;
+        background-color: #f9f9fa;
     }
 </style>
